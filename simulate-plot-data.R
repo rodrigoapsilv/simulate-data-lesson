db4b7eb97be7a7edd791637c76b4cf382c714b67
Rodrigo <rodrigoapsilva@arizona.edu>
2023-11-14 10:45:44 -0700

    change to polynomial relationship

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -6,6 +6,6 @@
 # Simulate predictor variable 
 x <- rnorm(n=100)
 # Simulate response variable with some noise
-y <- 2 *x + rnorm(n=100, sd=0.2)
+y <- x^2 + rnorm(n=100, sd=0.2)
 # Plot the data
 plot(x=x,y=y)
